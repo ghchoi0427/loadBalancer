e0d8871783756528ef723618b9c11795bfb59da1
ghchoi <ghchoi0427@gmail.com>
2022-11-23 23:58:55 +0900

    feat: generify method

--- src/main/java/util/JsonParser.java
+++ src/main/java/util/JsonParser.java
@@ -1,30 +1,15 @@
 package util;
 
 import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
-import domain.Member;
-
-import java.util.List;
-import java.util.Map;
 
 public class JsonParser {
     private static Gson gson = new Gson();
 
-    public static Map<String, Object> jsonToMap(String jsonStr) {
-        return gson.fromJson(jsonStr, new TypeToken<Map<String, Object>>() {
-        }.getType());
+    public static String objectToJson(Object object) {
+        return gson.toJson(object);
     }
 
-    public static String mapMemberListToJson(List<Member> members) {
-        return gson.toJson(members);
+    public static Object jsonToObject(String json) {
+        return gson.fromJson(json, Object.class);
     }
-
-    public static Member mapJsonStringToMember(String jsonStr) {
-        return gson.fromJson(jsonStr, Member.class);
-    }
-
-    public static String mapMemberObjectToJsonString(Member member) {
-        return gson.toJson(member);
-    }
-
 }
