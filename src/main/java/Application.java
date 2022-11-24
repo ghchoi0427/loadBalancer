565cf54fc7fe40cb0ff8271383171cfc66abfbb8
ghchoi <ghchoi0427@gmail.com>
2022-11-24 14:03:54 +0900

    feat: initiate ApiServiceContainer

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 public class Application {
     public static void main(String[] args) throws IOException {
         Configuration.init();
+        Configuration.initApiService();
         ServerApplication.run();
     }
 }
