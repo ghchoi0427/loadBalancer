ae3241468de52b550a63849e3079d36890e227f1
ghchoi <ghchoi0427@gmail.com>
2022-11-24 14:04:13 +0900

    feat: log

--- src/main/java/api/gateway/GateWay.java
+++ src/main/java/api/gateway/GateWay.java
@@ -1,7 +1,18 @@
 package api.gateway;
 
+import java.util.List;
+
 public class GateWay {
+    List<String> log;
     int port = 0;
+
+    public void appendLog(String message) {
+        log.add(message);
+    }
+
+    public List<String> getLog() {
+        return log;
+    }
 
     public GateWay(int port) {
         this.port = port;
