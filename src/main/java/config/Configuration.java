0d3e123f9dbcc5c4063a05725a78386cd876909c
ghchoi <ghchoi0427@gmail.com>
2022-11-24 13:36:04 +0900

    fix: uri entry

--- src/main/java/config/Configuration.java
+++ src/main/java/config/Configuration.java
@@ -3,6 +3,7 @@ package config;
 import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
 import container.ServerContainer;
+import handler.HttpRequestHandler;
 import util.ServerUtil;
 
 import java.io.IOException;
@@ -15,12 +16,12 @@ public class Configuration {
     }
 
     public static void init() throws IOException {
-        HttpServer server1 = HttpServer.create(new InetSocketAddress(8080), 0);
-        HttpServer server2 = HttpServer.create(new InetSocketAddress(8081), 0);
-        HttpServer server3 = HttpServer.create(new InetSocketAddress(8082), 0);
-        ServerUtil.initServer(server1);
-        ServerUtil.initServer(server2);
-        ServerUtil.initServer(server3);
+        HttpServer server1 = HttpServer.create(new InetSocketAddress(8081), 0);
+        HttpServer server2 = HttpServer.create(new InetSocketAddress(8082), 0);
+        HttpServer server3 = HttpServer.create(new InetSocketAddress(8083), 0);
+        ServerUtil.initServer(server1, "/api", new HttpRequestHandler(1));
+        ServerUtil.initServer(server2, "/api", new HttpRequestHandler(2));
+        ServerUtil.initServer(server3, "/api", new HttpRequestHandler(3));
         ServerContainer.getInstance().addServerInstance(server1);
         ServerContainer.getInstance().addServerInstance(server2);
         ServerContainer.getInstance().addServerInstance(server3);
