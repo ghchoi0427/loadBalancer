d303733ad34941cf581aa13ce94478b5a601437b
ghchoi <ghchoi0427@gmail.com>
2022-11-24 13:36:17 +0900

    feat: add field

--- src/main/java/api/gateway/GateWay.java
+++ src/main/java/api/gateway/GateWay.java
@@ -1,4 +1,17 @@
 package api.gateway;
 
 public class GateWay {
+    int port = 0;
+
+    public GateWay(int port) {
+        this.port = port;
+    }
+
+    public int getPort() {
+        return port;
+    }
+
+    public void setPort(int port) {
+        this.port = port;
+    }
 }
