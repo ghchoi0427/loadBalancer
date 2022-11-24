7bf517baff8f7bc6be4018fb836b09fd9d739665
ghchoi <ghchoi0427@gmail.com>
2022-11-24 13:37:40 +0900

    fix: receive handler as parameter

--- src/main/java/util/ServerUtil.java
+++ src/main/java/util/ServerUtil.java
@@ -1,13 +1,13 @@
 package util;
 
+import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
 import config.Configuration;
-import handler.HttpRequestHandler;
 
 public class ServerUtil {
 
-    public static void initServer(HttpServer server) {
-        Configuration.bindHandler(server, "/", new HttpRequestHandler());
+    public static void initServer(HttpServer server, String path, HttpHandler handler) {
+        Configuration.bindHandler(server, path, handler);
         server.setExecutor(null);
         System.out.println("server initiated");
     }
