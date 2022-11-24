4b168d147110fca6c446673ed97439f8b009ae0d
ghchoi <ghchoi0427@gmail.com>
2022-11-24 12:43:15 +0900

    fix: run server instances in ServerContainer

--- src/main/java/ServerApplication.java
+++ src/main/java/ServerApplication.java
@@ -1,21 +1,14 @@
 import com.sun.net.httpserver.HttpServer;
-import config.Configuration;
-import handler.HttpRequestHandler;
+import container.ServerContainer;
 
-import java.io.IOException;
-import java.net.InetSocketAddress;
+import java.util.HashMap;
 
 public class ServerApplication {
     public static void run() {
-        HttpServer server;
-        try {
-            server = HttpServer.create(new InetSocketAddress(8080), 0);
-            Configuration.bindHandler(server, "/", new HttpRequestHandler());
-            server.setExecutor(null);
-            server.start();
-            System.out.println("server started");
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+        HashMap<Integer, HttpServer> serverMap = ServerContainer.getInstance().getServerInstances();
+        for (int i = 0; i < serverMap.size(); i++) {
+            serverMap.get(i).start();
+            System.out.println("server " + i + " started");
         }
     }
 
