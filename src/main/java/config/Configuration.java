be61ff9d1f8507da7ea5c7b59c30f874703d0238
ghchoi <ghchoi0427@gmail.com>
2022-11-24 12:42:41 +0900

    feat: put server instances in ServerContainer

--- src/main/java/config/Configuration.java
+++ src/main/java/config/Configuration.java
@@ -2,10 +2,27 @@ package config;
 
 import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
+import container.ServerContainer;
+import util.ServerUtil;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
 
 public class Configuration {
 
     public static void bindHandler(HttpServer server, String path, HttpHandler handler) {
         server.createContext(path, handler);
+    }
+
+    public static void init() throws IOException {
+        HttpServer server1 = HttpServer.create(new InetSocketAddress(8080), 0);
+        HttpServer server2 = HttpServer.create(new InetSocketAddress(8081), 0);
+        HttpServer server3 = HttpServer.create(new InetSocketAddress(8082), 0);
+        ServerUtil.initServer(server1);
+        ServerUtil.initServer(server2);
+        ServerUtil.initServer(server3);
+        ServerContainer.getInstance().addServerInstance(server1);
+        ServerContainer.getInstance().addServerInstance(server2);
+        ServerContainer.getInstance().addServerInstance(server3);
     }
 }
