90d19a595065fa9fc81aa5b3fe5d356ee6f46e81
ghchoi <ghchoi0427@gmail.com>
2022-11-24 13:37:21 +0900

    feat: start front server

--- src/main/java/ServerApplication.java
+++ src/main/java/ServerApplication.java
@@ -1,15 +1,27 @@
 import com.sun.net.httpserver.HttpServer;
 import container.ServerContainer;
+import handler.FrontServerHandler;
+import policy.RoundRobin;
+import util.ServerUtil;
 
+import java.io.IOException;
+import java.net.InetSocketAddress;
 import java.util.HashMap;
 
 public class ServerApplication {
-    public static void run() {
+    public static void run() throws IOException {
+        startFrontServer();
         HashMap<Integer, HttpServer> serverMap = ServerContainer.getInstance().getServerInstances();
         for (int i = 0; i < serverMap.size(); i++) {
             serverMap.get(i).start();
             System.out.println("server " + i + " started");
         }
+    }
+
+    private static void startFrontServer() throws IOException {
+        HttpServer frontServer = HttpServer.create(new InetSocketAddress(8080), 0);
+        ServerUtil.initServer(frontServer, "/", new FrontServerHandler(new RoundRobin()));
+        frontServer.start();
     }
 
 
