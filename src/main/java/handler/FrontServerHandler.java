307089173cb3b7dc1d1d40334992be3ffbf9b5a0
ghchoi <ghchoi0427@gmail.com>
2022-11-24 13:35:46 +0900

    feat: create FrontServerHandler

---
+++ src/main/java/handler/FrontServerHandler.java
@@ -0,0 +1,31 @@
+package handler;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import policy.LoadBalancingPolicy;
+import util.RequestSender;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
+public class FrontServerHandler implements HttpHandler {
+    private final String LOCAL_URL = "http://127.0.0.1:";
+
+    RequestSender sender = new RequestSender();
+    private final LoadBalancingPolicy loadBalancingPolicy;
+
+    public FrontServerHandler(LoadBalancingPolicy loadBalancingPolicy) {
+        this.loadBalancingPolicy = loadBalancingPolicy;
+    }
+
+    @Override
+    public void handle(HttpExchange arg) throws IOException {
+        arg.getResponseHeaders().add("Access-Control-Allow-Origin", "*");
+        int gatewayPort = loadBalancingPolicy.gateway().getPort();
+        String result = sender.send(LOCAL_URL + gatewayPort + "/api" + arg.getRequestURI(), arg.getRequestMethod());
+        arg.sendResponseHeaders(200, result.length());
+        OutputStream os = arg.getResponseBody();
+        os.write(result.getBytes());
+        os.close();
+    }
+}
