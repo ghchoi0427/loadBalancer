470e1849697120ded68fe14cec9beb633bca4be0
ghchoi <ghchoi0427@gmail.com>
2022-11-24 13:38:52 +0900

    feat: load all members

--- src/main/java/controller/MemberController.java
+++ src/main/java/controller/MemberController.java
@@ -4,6 +4,8 @@ import domain.Member;
 import service.MemberService;
 import util.JsonParser;
 
+import java.util.List;
+
 public class MemberController implements Controller {
     private final MemberService memberService;
 
@@ -19,5 +21,10 @@ public class MemberController implements Controller {
     public String doGet(Long id) {
         Member memberById = memberService.findMemberById(id);
         return JsonParser.objectToJson(memberById);
+    }
+
+    public String doGet() {
+        List<Member> allMembers = memberService.getAllMembers();
+        return JsonParser.objectToJson(allMembers);
     }
 }
