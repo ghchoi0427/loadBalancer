bbd18f308c5e6372e994b5a74ab4de2651f8430c
ghchoi <ghchoi0427@gmail.com>
2022-11-27 15:32:27 +0900

    feat: renew project

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,13 +1,5 @@
-import config.Configuration;
-import util.TestSetting;
-
-import java.io.IOException;
-
 public class Application {
-    public static void main(String[] args) throws IOException {
-        Configuration.init();
-        Configuration.initApiService();
-        TestSetting.fillDatabase();
-        ServerApplication.run();
+    public static void main(String[] args) {
+
     }
 }
