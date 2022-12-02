eefe359bcdfc5b21a2edd37d6bb6128584d3a661
ghchoi <ghchoi0427@gmail.com>
2022-12-02 21:53:38 +0900

    feat: create MyServer

---
+++ src/main/java/api/MyServer.java
@@ -0,0 +1,49 @@
+package api;
+
+import com.sun.net.httpserver.HttpExchange;
+import config.Configuration;
+import domain.ServerLog;
+import util.UriParser;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+
+public class MyServer {
+    private final int port;
+    private List<ServerLog> logs;
+
+    public int getPort() {
+        return port;
+    }
+
+    public List<ServerLog> getLog() {
+        return logs;
+    }
+
+    public void addLog(ServerLog serverLog) {
+        logs.add(serverLog);
+    }
+
+    public MyServer(int port) {
+        this.port = port;
+    }
+
+    public void start() {
+        System.out.println("Server [" + port + "] : started");
+    }
+
+    public void handle(HttpExchange arg) throws IOException {
+        String[] uri = UriParser.parseURI(arg.getRequestURI().toString());
+
+        try {
+            String apiUri = uri[1];
+            ApiService service = Configuration.apiServiceContainer().getApiMap().get(apiUri);
+            service.handle(arg);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            arg.sendResponseHeaders(400, e.getMessage().length());
+            arg.getResponseBody().write(e.getMessage().getBytes(StandardCharsets.UTF_8));
+        }
+    }
+}
