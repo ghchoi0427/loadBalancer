5070fd08aadf737759a3f3aa1964d94acee1fe7b
ghchoi <ghchoi0427@gmail.com>
2022-12-02 21:55:25 +0900

    feat: implement ServerApplication

---
+++ src/main/java/ServerApplication.java
@@ -0,0 +1,27 @@
+import api.FrontServer;
+import api.MyServer;
+import config.Configuration;
+import handler.FrontServerHandler;
+
+import java.io.IOException;
+
+public class ServerApplication {
+    private static final int frontServerPort = 8090;
+    private static final FrontServerHandler frontServerHandler = new FrontServerHandler();
+
+    public static void run() throws IOException {
+        Configuration.fillServerContainer();
+        Configuration.fillApiServiceContainer();
+        startFrontServer();
+        startServer();
+    }
+
+    private static void startFrontServer() throws IOException {
+        FrontServer frontServer = new FrontServer(frontServerPort, frontServerHandler);
+        frontServer.initServer();
+    }
+
+    private static void startServer() {
+        Configuration.myServerContainer().getServerHashMap().values().forEach(MyServer::start);
+    }
+}
