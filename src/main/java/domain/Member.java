aed7f4e9c0a6788621f6fb33d730267f2d2f7d82
ghchoi <ghchoi0427@gmail.com>
2022-12-03 00:05:17 +0900

    feat: create entity class Member

---
+++ src/main/java/domain/Member.java
@@ -0,0 +1,36 @@
+package domain;
+
+public class Member {
+    private Long id;
+    private String name;
+    private String profile;
+
+    public Member(String name, String profile) {
+        this.name = name;
+        this.profile = profile;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getProfile() {
+        return profile;
+    }
+
+    public void setProfile(String profile) {
+        this.profile = profile;
+    }
+}
