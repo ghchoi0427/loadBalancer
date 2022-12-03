8557e2b4811f5ea2674b9bb31fe317e876b7eedd
ghchoi <ghchoi0427@gmail.com>
2022-12-03 12:25:38 +0900

    feat: LeastConnection 생성

---
+++ src/main/java/policy/LeastConnection.java
@@ -0,0 +1,45 @@
+package policy;
+
+import api.ApiService;
+import api.MyServer;
+import config.Configuration;
+import container.MyServerContainer;
+import domain.ServerLog;
+
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+
+public class LeastConnection implements LoadBalancingPolicy {
+    private final MyServerContainer serverContainer = Configuration.myServerContainer();
+    private int range = 10;
+
+    @Override
+    public MyServer myServer() {
+        return serverContainer.getServerHashMap().values().stream().min((o1, o2) -> {
+            int o1Count = getCurrentConnectionCount(o1, range);
+            int o2Count = getCurrentConnectionCount(o2, range);
+            if (o1Count < o2Count) {
+                return 0;
+            } else return 1;
+        }).get();
+
+    }
+
+    @Override
+    public ApiService apiService() {
+        return null;
+    }
+
+    private int getCurrentConnectionCount(MyServer server, int range) {
+        int result = 0;
+        LocalDateTime now = LocalDateTime.now();
+        for (ServerLog log : server.getLog()) {
+            LocalDateTime from = log.getCreatedTime();
+            long seconds = ChronoUnit.SECONDS.between(from, now);
+            if (seconds < range) {
+                result++;
+            }
+        }
+        return result;
+    }
+}
