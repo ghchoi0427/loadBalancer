da7b87f0d30f2234f05d659f8659d5fe2a1a0d55
ghchoi <ghchoi0427@gmail.com>
2022-12-04 13:52:38 +0900

    fix: remove system output

--- src/main/java/util/MessageSender.java
+++ src/main/java/util/MessageSender.java
@@ -7,14 +7,11 @@ import java.net.Socket;
 import java.nio.charset.StandardCharsets;
 
 public class MessageSender implements Runnable {
-
     private final Socket from;
-
 
     public MessageSender(Socket from) {
         this.from = from;
     }
-
 
     @Override
     public void run() {
@@ -25,7 +22,6 @@ public class MessageSender implements Runnable {
                 if (readBytes > 0) {
                     String message = new String(bytes, 0, readBytes, StandardCharsets.UTF_8);
                     Logger.writeFile(message);
-                    System.out.print(message);
                 }
             } catch (IOException e) {
                 throw new RuntimeException(e);
